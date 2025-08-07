4d42fccc95d03a83af2bbc1a4c6306aa1b0e24b0
Vighnesh Shinde <vighneshshinde80@gmail.com>
2025-08-07 23:44:22 +0530

    try to access it also on mobile

--- server/controllers/authControllers.js
+++ server/controllers/authControllers.js
@@ -85,8 +85,8 @@ exports.loginUser = async (req, res, next) => {
     
     res.cookie('token', token, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === 'production',
-      sameSite: 'strict',
+      secure: true,
+      sameSite: 'none',
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
      
